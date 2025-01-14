7b8f85e175a005c108a8e52ff7911f13a649c43a
ar4487 <112604187+ar4487@users.noreply.github.com>
2025-01-14 08:18:34 +0000

    completed loop

---
+++ 05_iteration/loop.js
@@ -0,0 +1,33 @@
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    if(element==5){
+       // console.log("5 is best nmber");
+        
+    }
+    //console.log(element);
+    
+    
+}
+for (let i = 0; i <=10; i++) {
+   // console.log(`outer loop ${i}`);
+    for (let j = 0; j <= 10; j++) {
+       // console.log(i + '*' +j + ' = ' + i*j);
+        
+    }}
+    // let array=['superman','batman','flash']
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+    //    // console.log(element);
+        
+    // }
+
+    for (let index = 1; index <= 20; index++) {
+       if(index==5) {
+            console.log('Dectected 5');
+            //continue;
+            
+        }
+        console.log(`value of i is: ${index} `);
+        
+        
+    }
